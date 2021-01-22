64d337af5ff724f5228f746a075cbcb464984851
sahil705 <sahil.sharma@lngconsultancy.co.in>
2021-01-22 18:05:04 +0530

    Add project files.

---
+++ LearningGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
